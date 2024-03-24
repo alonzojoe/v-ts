068a021c28db82587d40d70195f451297116848d
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-03-24 21:55:23 +0800

    package setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/lara-dark-green/theme.css'
+import 'primeflex/primeflex.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue)
+app.mount('#app');
---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.VITE_BASE_URL),
+    routes
+})
+
+const routes = [
+
+]
+
+
+export default router
